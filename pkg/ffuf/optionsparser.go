645baadd815649b04c82aeb3f90b80c6a80a4f00
DoI <5291556+denandz@users.noreply.github.com>
2026-07-26 20:16:08 +1200

    Update optionsparser.go
    Increase default timeout and change default threads to 1. Makes ffuf timing analysis more reliable by default when fuzzing and searching for outliers

--- pkg/ffuf/optionsparser.go
+++ pkg/ffuf/optionsparser.go
@@ -143,7 +143,7 @@ func NewConfigOptions() *ConfigOptions {
 	c.General.StopOn403 = false
 	c.General.StopOnAll = false
 	c.General.StopOnErrors = false
-	c.General.Threads = 5
+	c.General.Threads = 1
 	c.General.Verbose = false
 	c.HTTP.Data = ""
 	c.HTTP.FollowRedirects = false
@@ -155,7 +155,7 @@ func NewConfigOptions() *ConfigOptions {
 	c.HTTP.RecursionDepth = 0
 	c.HTTP.RecursionStrategy = "default"
 	c.HTTP.ReplayProxyURL = ""
-	c.HTTP.Timeout = 10
+	c.HTTP.Timeout = 30
 	c.HTTP.SNI = ""
 	c.HTTP.URL = ""
 	c.HTTP.Http2 = false
